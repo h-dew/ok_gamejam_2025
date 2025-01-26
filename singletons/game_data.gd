529871686c1c7759d7207bc6b1447c42f53a6bb2
Spencer Stephen <spncrstphn14@gmail.com>
2025-01-26 10:28:21 -0800

    small td changes, merge conflict waiting to happen

---
+++ singletons/game_data.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var tower_data = {
+	"square_t1": {
+		"damage": 20,
+		"rate": 1,
+		"range": 400},
+	"square_t2": {
+		"damage": 40,
+		"rate": 1.5,
+		"range": 500},
+	"circle_t1": {
+		"damage": 10,
+		"rate": 3,
+		"range": 300},
+	"circle_t2": {
+		"damage": 15,
+		"rate": 4,
+		"range": 300}
+}
