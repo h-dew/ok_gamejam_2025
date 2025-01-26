84a6bfdaeb36e45ddc679433c147183290250ee0
Spencer Stephen <spncrstphn14@gmail.com>
2025-01-26 13:27:19 -0800

    bubble collision stuff needs repurposing

--- singletons/game_data.gd
+++ singletons/game_data.gd
@@ -2,19 +2,19 @@ extends Node
 
 var tower_data = {
 	"square_t1": {
-		"damage": 20,
+		"damage": 50,
 		"rate": 1,
 		"range": 400},
 	"square_t2": {
 		"damage": 40,
-		"rate": 1.5,
+		"rate": 0.9,
 		"range": 500},
 	"circle_t1": {
 		"damage": 10,
-		"rate": 2,
+		"rate": 1.5,
 		"range": 300},
 	"circle_t2": {
 		"damage": 15,
-		"rate": 4,
+		"rate": 1.2,
 		"range": 300}
 }
