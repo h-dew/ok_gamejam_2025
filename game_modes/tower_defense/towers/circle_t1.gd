6b603174525e867dbc91d21f6cbf3cdbaf0c842e
h-dew <benjaminscown@gmail.com>
2025-01-26 11:55:48 -0800

    added better tower behaviour

--- game_modes/tower_defense/towers/circle_t1.gd
+++ game_modes/tower_defense/towers/circle_t1.gd
@@ -1,8 +1,12 @@
 extends "res://game_modes/tower_defense/towers/turrets.gd"
+	
+func _ready() -> void:
+	damage = float(GameData.tower_data["circle_t1"]["damage"]) / 128.0
+	rate = int((1.0 / float(GameData.tower_data["circle_t1"]["rate"])) * 90)
+	
+	wavetex = $"../../../../GameScene".wavetex
+	pos = get_position()
 
 func _process(_delta: float) -> void:
-	var wavetex = $"../../../../GameScene".wavetex
-	var pos = get_position()
-	
-	if Engine.get_frames_drawn() % 60 == 0:
-		wavetex.drawSquare(0.1, Vector2i(pos.x / 6, pos.y / 6), Vector2i(2, 2), Vector2i(0,0))
+	if Engine.get_frames_drawn() % rate == 0:
+		wavetex.drawSquare(damage, Vector2i(pos.x / 6, pos.y / 6), Vector2i(2, 2), Vector2i(0,0))
