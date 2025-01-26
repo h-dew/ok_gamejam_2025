f17c2249c79337f383c960d6256b1e1b01aad8cf
Spencer Stephen <spncrstphn14@gmail.com>
2025-01-26 12:21:16 -0800

    local game scene

--- game_modes/tower_defense/game_scene.gd
+++ game_modes/tower_defense/game_scene.gd
@@ -5,15 +5,14 @@ var build_valid = false
 var build_location
 var build_type
 
+var current_wave = 0
+var enemies_in_wave = 0
+
 func _ready():
 	print(get_tree().get_nodes_in_group("tower_buttons"))
 	for i in get_tree().get_nodes_in_group("tower_buttons"):
 		i.pressed.connect(self.initiate_build_mode.bind(i.get_name()))
-<<<<<<< HEAD
 	start_next_wave()	
-=======
-	wavetex = $"../SubViewportContainer/SubViewport/TextureRect"
->>>>>>> 13ee113a70dbfa64cda302c6ecc47b2911393195
 	
 func _process(delta):
 	if build_mode:
@@ -25,6 +24,31 @@ func _input(event):
 	if event.is_action_released("td_accept") and build_mode == true:
 		verify_and_build()
 		cancel_build_mode()
+
+# Bubble level functions
+
+func start_next_wave():
+	var wave_data = retrieve_wave_data()
+	await get_tree().create_timer(0.2).timeout
+	spawn_bubbles(wave_data)
+	
+	
+func retrieve_wave_data():
+	var wave_data = [["bubble", 1.2], ["bubble", 0.6]]
+	current_wave += 1
+	enemies_in_wave = wave_data.size()
+	return wave_data
+	
+func spawn_bubbles(wave_data):
+	for i in wave_data:
+		print(i)
+		var new_enemy = preload("res://game_modes/tower_defense/bubbles/bubble.tscn").instantiate()
+		get_node("Map").get_node("Path").add_child(new_enemy, true)
+		
+		await get_tree().create_timer(i[1]).timeout
+
+
+# Tower Building Functions
 	
 func initiate_build_mode(tower_type):
 	if build_mode:
