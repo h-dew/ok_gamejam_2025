eade2e6ed75bfb2b48ea10c488ab2db9571f4e25
Spencer Stephen <spncrstphn14@gmail.com>
2025-01-26 12:06:17 -0800

    bubblebobble

--- game_modes/tower_defense/towers/circle_t1.gd
+++ game_modes/tower_defense/towers/circle_t1.gd
@@ -1,7 +1,7 @@
 extends "res://game_modes/tower_defense/towers/turrets.gd"
 
 func _process(_delta: float) -> void:
-	var wavetex = $"../../../../GameScene".wavetex
+	var wavetex = "res://game_modes/tower_defense/texture_rect.gd"
 	var pos = get_position()
 	
 	if Engine.get_frames_drawn() % 60 == 0:
