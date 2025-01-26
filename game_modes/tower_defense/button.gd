1735bb978d1ff0cda88a493e15bd8e693aaa4e51
h-dew <benjaminscown@gmail.com>
2025-01-26 02:46:18 -0800

    added main background texture and wave generation script

--- game_modes/tower_defense/button.gd
+++ game_modes/tower_defense/button.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _pressed() -> void:
-	var post = $"../SubViewportContainer/SubViewport/TextureRect".drawSquare(Vector2i(160, 100), Vector2i(3, 3))
+	var post = $"../SubViewportContainer/SubViewport/TextureRect".drawSquare(0.1, Vector2i(160, 100), Vector2i(1, 1), Vector2i(10, 10))
