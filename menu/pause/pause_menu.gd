4a868cbc3e8c2a9accee9d21830253cb67e6d824
h-dew <benjaminscown@gmail.com>
2025-01-26 01:09:11 -0800

    wave function completed. displays. crowd shocked.

--- menu/pause/pause_menu.gd
+++ menu/pause/pause_menu.gd
@@ -1,13 +1,4 @@
 extends Control
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _on_pause_button_pressed():
 	get_tree().paused = true
